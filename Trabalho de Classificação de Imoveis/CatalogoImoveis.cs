03984fadd0f63389f8c9cb2567d5351542044d9b
luiza2501 <efigenia.l@escolar.ifrn.edu.br>
2023-10-20 17:16:02 -0300

    Adição de comentários a classe CatalogoImoveis

--- Trabalho de Classificação de Imoveis/CatalogoImoveis.cs
+++ Trabalho de Classificação de Imoveis/CatalogoImoveis.cs
@@ -1,13 +1,14 @@
 // criei a classe catalogoimoveis 
 public class CatalogoImoveis
 {
+//declarei uma variavel publica, uma lista
     public List<Imovel> imoveis;
 
-
+//criei um construtor pra classe
     public CatalogoImoveis(){
+        //inicializando a lista imovel
         this.imoveis = new List<Imovel>(); 
     }
-
     // criei o metodo para adicionar um imóvel
     public void Adicionar(Imovel novoImovel)
     {
@@ -19,9 +20,10 @@ public class CatalogoImoveis
     {
         imoveis.Remove(imovel);
     }
-
+//Declarei o método VerificarCatalogo
      public void VerificarCatalogo()
     {
+        //Usei o metodo de repetição foreach iniciar a linha de codigo abaixo que vai ser repetido a cada Imovel na lista
         foreach (var imovel in imoveis)
         {
             Console.WriteLine($"ID: {imovel.Id}, Endereço: {imovel.Endereco}, Preço: {imovel.Preco}, Metros Quadrados: {imovel.MetrosQuadrados}.");
